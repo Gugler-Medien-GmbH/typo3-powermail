4b2b17ea2c3de8ea5e3fe582cda5bbdf5119ec26
Markus Ketterer <markus.ketterer@gugler.at>
2025-09-01 09:18:26 +0200

    revet changes...

--- Classes/Domain/Validator/SpamShield/CaptchaEUMethod.php
+++ Classes/Domain/Validator/SpamShield/CaptchaEUMethod.php
@@ -16,111 +16,111 @@ use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
  */
 class CaptchaEUMethod extends AbstractMethod
 {
-  /**
-   * @var string
-   */
-  protected $restKey = '';
+    /**
+     * @var string
+     */
+    protected $restKey = '';
 
-  /**
-   * Check if secret key is given and set it
-   *
-   * @return void
-   * @throws \Exception
-   */
-  public function initialize(): void
-  {
-    if ($this->isFormWithCaptchaEUField()) {
+    /**
+     * Check if secret key is given and set it
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function initialize(): void
+    {
+        if ($this->isFormWithCaptchaEUField()) {
 
-      if (empty($this->configuration['restkey']) || $this->configuration['restkey'] === 'abcdef') {
-        throw new \LogicException(
-          'No restkey given. Please add a secret key to TypoScript Constants',
-          1607014176
-        );
-      }
-      $this->restKey = $this->configuration['restkey'];
+            if (empty($this->configuration['restkey']) || $this->configuration['restkey'] === 'abcdef') {
+                throw new \LogicException(
+                    'No restkey given. Please add a secret key to TypoScript Constants',
+                    1607014176
+                );
+            }
+            $this->restKey = $this->configuration['restkey'];
 
-    }
-  }
-
-  public function checkSolution($solution)
-  {
-    $ch = curl_init("https://www.captcha.eu/validate");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $solution);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Rest-Key: ' . $this->restKey));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $result = curl_exec($ch);
-    curl_close($ch);
-
-    $resultObject = json_decode($result);
-    if ($resultObject->success) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-
-  /**
-   * @return bool true if spam recognized
-   * @throws Exception
-   * @throws ExtensionConfigurationExtensionNotConfiguredException
-   * @throws ExtensionConfigurationPathDoesNotExistException
-   */
-  public function spamCheck(): bool
-  {
-
-    if (!$this->isFormWithCaptchaEUField() || $this->isCaptchaCheckToSkip() || !isset($_POST["captcha_at_solution"])) {
-      return false;
-    }
-    $result = $this->checkSolution($_POST["captcha_at_solution"]);
-    if (!$result) {
-      return true;
-    }
-    return false;
-  }
-
-  /**
-   * Check if current form has a recaptcha field
-   *
-   * @return bool
-   * @throws ExtensionConfigurationExtensionNotConfiguredException
-   * @throws ExtensionConfigurationPathDoesNotExistException
-   * @throws Exception
-   */
-  protected function isFormWithCaptchaEUField(): bool
-  {
-    foreach ($this->mail->getForm()->getPages() as $page) {
-      /** @var Field $field */
-      foreach ($page->getFields() as $field) {
-        if ($field->getType() === 'captchaeu') {
-          return true;
         }
-      }
     }
-    return false;
-  }
 
+    public function checkSolution($solution)
+    {
+        $ch = curl_init("https://www.captcha.eu/validate");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $solution);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Rest-Key: ' . $this->restKey));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
 
-  /**
-   * Captcha check should be skipped on createAction if there was a confirmationAction where the captcha was
-   * already checked before
-   * Note: $this->flexForm is only available in powermail 3.9 or newer
-   *
-   * @return bool
-   */
-  protected function isCaptchaCheckToSkip(): bool
-  {
-    if (property_exists($this, 'flexForm')) {
-      $confirmationActive = $this->flexForm['settings']['flexform']['main']['confirmation'] === '1';
-      return $this->getActionName() === 'create' && $confirmationActive;
+        $resultObject = json_decode($result);
+        if ($resultObject->success) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    return false;
-  }
 
 
-  protected function getActionName(): string
-  {
-    $pluginVariables = $GLOBALS['TYPO3_REQUEST']->getQueryParams()['tx_powermail_pi1'];
-    return $pluginVariables['action'];
-  }
+    /**
+     * @return bool true if spam recognized
+     * @throws Exception
+     * @throws ExtensionConfigurationExtensionNotConfiguredException
+     * @throws ExtensionConfigurationPathDoesNotExistException
+     */
+    public function spamCheck(): bool
+    {
+
+        if (!$this->isFormWithCaptchaEUField() || $this->isCaptchaCheckToSkip()) {
+            return false;
+        }
+        $result = $this->checkSolution($_POST["captcha_at_solution"]);
+        if (!$result) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Check if current form has a recaptcha field
+     *
+     * @return bool
+     * @throws ExtensionConfigurationExtensionNotConfiguredException
+     * @throws ExtensionConfigurationPathDoesNotExistException
+     * @throws Exception
+     */
+    protected function isFormWithCaptchaEUField(): bool
+    {
+        foreach ($this->mail->getForm()->getPages() as $page) {
+            /** @var Field $field */
+            foreach ($page->getFields() as $field) {
+                if ($field->getType() === 'captchaeu') {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Captcha check should be skipped on createAction if there was a confirmationAction where the captcha was
+     * already checked before
+     * Note: $this->flexForm is only available in powermail 3.9 or newer
+     *
+     * @return bool
+     */
+    protected function isCaptchaCheckToSkip(): bool
+    {
+        if (property_exists($this, 'flexForm')) {
+            $confirmationActive = $this->flexForm['settings']['flexform']['main']['confirmation'] === '1';
+            return $this->getActionName() === 'create' && $confirmationActive;
+        }
+        return false;
+    }
+
+
+    protected function getActionName(): string
+    {
+        $pluginVariables = $GLOBALS['TYPO3_REQUEST']->getQueryParams()['tx_powermail_pi1'];
+        return $pluginVariables['action'];
+    }
 }
